e8ffeaa8e3689e96428223d327d229ff66d3afec
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-27 08:27:37 +0200

    Set on click function for deleting sensor and refresh data grid view

--- sensors-with-entity-framework/SensorsForm.cs
+++ sensors-with-entity-framework/SensorsForm.cs
@@ -26,6 +26,30 @@ namespace sensors_with_entity_framework
             addSensorForm.ShowDialog();
         }
 
+        private void btnDeleteSensor_Click(object sender, EventArgs e)
+        {
+            Sensors sensor = dgcSensors.CurrentRow.DataBoundItem as Sensors;
+
+            using (var db = new DB_EntityEntities1())
+            {
+                db.Sensors.Attach(sensor);
+
+                List<SensorReadings> sensorReadings = db.SensorReadings.ToList().Where(reading => reading.IdSensors == sensor.Id).ToList();
+
+                foreach (SensorReadings item in sensorReadings)
+                {
+                    db.SensorReadings.Attach(item);
+                    db.SensorReadings.Remove(item);
+                }
+
+                db.Sensors.Remove(sensor);
+
+                db.SaveChanges();
+            }
+
+            refreshDataGrid();
+        }
+
         public List<Sensors> getSensors()
         {
             List<Sensors> sensors = null;
